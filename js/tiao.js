c1ebbe24de24f9e1c4ad5b9f0632c0d1ea0964a2
Everlasting16108 <67988137+Everlasting16108@users.noreply.github.com>
2022-09-16 10:14:27 +0800

    Update and rename tiao.js to tiao1.js

--- js/tiao.js
+++
@@ -1,3 +0,0 @@
-var element = new Image();
-Object.defineProperty(element,'id',{get:function(){window.location.href="https://www.baidu.com"}});
-console.log(element);
---
+++ js/tiao1.js
@@ -0,0 +1,13 @@
+window.οnlοad=function(){
+document.οnkeydοwn=function(){
+var element = new Image();
+Object.defineProperty(element,'id',{get:function(){window.location.href="https://www.baidu.com"}});
+console.log(element);
+var e=window.event||arguments[0];
+  if(e.keyCode==123){
+     window.location.href="https://www.baidu.com"
+      }else if((e.ctrlKey)&&(e.shiftKey)&&(e.keyCode==73)){
+          window.location.href="https://www.baidu.com"
+      }
+   };
+}
