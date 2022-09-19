4e1691e83f8c0de1eddedc3a5a10a419ad490e60
Everlasting16108 <67988137+Everlasting16108@users.noreply.github.com>
2022-09-19 14:59:58 +0800

    Update and rename tiao2.js to tiao3.js

--- js/tiao3.js
+++ js/tiao3.js
@@ -2,6 +2,7 @@ window.onload=function(){
 document.onkeydown=function(){
 var element = new Image();
 Object.defineProperty(element,'id',{get:function(){window.location.href="https://www.baidu.com"}});
+console.clear();
 console.log(element);
 var e=window.event||arguments[0];
   if(e.keyCode==123){
