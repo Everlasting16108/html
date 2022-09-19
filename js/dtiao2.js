8b091670a8cfc82a2008e14821b499f9890408dc
Everlasting16108 <67988137+Everlasting16108@users.noreply.github.com>
2022-09-19 11:06:43 +0800

    Update and rename dtiao1.js to dtiao2.js

--- js/dtiao2.js
+++ js/dtiao2.js
@@ -1,11 +1,10 @@
 setInterval(function() {
-  document.body.innerHTML="发生错误，请重试！";
   console.clear();
-  alert("发生错误，请重试！")
   check();
 }, 2000);
 var check = function() {
   function doCheck(a) {
+    document.body.innerHTML="发生错误，请重试！";
     if (('' + a / a)['length'] !== 1 || a % 20 === 0) {
       (function() {}['constructor']('debugger')());
     } else {
