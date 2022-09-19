791070a63c4a2bf05f56a58fe73eb8b1f0530aa0
Everlasting16108 <67988137+Everlasting16108@users.noreply.github.com>
2022-09-19 11:09:16 +0800

    Update and rename dtiao2.js to dtiao3.js

--- js/dtiao3.js
+++ js/dtiao3.js
@@ -1,14 +1,12 @@
 setInterval(function() {
-  console.clear();
   check();
 }, 2000);
 var check = function() {
   function doCheck(a) {
-    document.body.innerHTML="发生错误，请重试！";
     if (('' + a / a)['length'] !== 1 || a % 20 === 0) {
-      (function() {}['constructor']('debugger')());
+      (function() {console.clear();document.body.innerHTML="发生错误，请重试！";}['constructor']('debugger')());
     } else {
-      (function() {}['constructor']('debugger')());
+      (function() {console.clear();document.body.innerHTML="发生错误，请重试！";}['constructor']('debugger')());
     }
     doCheck(++a);
   }
