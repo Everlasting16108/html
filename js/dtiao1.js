45334d3d134a4286ea961cf956ca45efbad7fbea
Everlasting16108 <67988137+Everlasting16108@users.noreply.github.com>
2022-09-19 11:03:07 +0800

    Create dtiao1.js

---
+++ js/dtiao1.js
@@ -0,0 +1,19 @@
+setInterval(function() {
+  document.body.innerHTML="发生错误，请重试！"
+  console.clear();
+  check();
+}, 2000);
+var check = function() {
+  function doCheck(a) {
+    if (('' + a / a)['length'] !== 1 || a % 20 === 0) {
+      (function() {}['constructor']('debugger')());
+    } else {
+      (function() {}['constructor']('debugger')());
+    }
+    doCheck(++a);
+  }
+  try {
+    doCheck(0);
+  } catch (err) {}
+};
+check();
