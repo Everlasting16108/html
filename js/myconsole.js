a9c7dd1ce84f1f0388cb284eb96033d2d66d03b9
Everlasting16108 <67988137+Everlasting16108@users.noreply.github.com>
2022-11-17 14:08:01 +0800

    Update myconsole.js

--- js/myconsole.js
+++ js/myconsole.js
@@ -3,7 +3,7 @@ window.onresize=checkSB;
          // 判断是否打开调试台
          if(window.outerWidth - window.innerWidth > 50 || window.outerHeight - window.innerHeight > 200){
             console.clear();
-            document.write(`请所在你切勿尝试使用调试，再次触发将记录IP进入黑名单<a href="/" style="color:#4285f4;">点击返回</a>`);
+            document.write(`请你切勿尝试使用调试，再次触发将记录IP进入黑名单<a href="/" style="color:#4285f4;">点击返回</a>`);
          }
        };
        checkSB();
